ad6c8432aaaf688365619e6e90b50a061255058f
Bartosz Nowak <sztosz.github@gmail.com>
2017-07-30 11:35:29 +0200

    Update build.ml

--- pkg/build.ml
+++ pkg/build.ml
@@ -6,17 +6,18 @@
 let () =
   Pkg.describe "BetterErrors" ~builder:(`OCamlbuild []) [
     Pkg.lib "pkg/META";
-    Pkg.bin ~auto:true "src/betterErrorsShell" ~dst:"huh";
+    Pkg.bin ~auto:true "src/BetterErrors" ~dst:"berror";
     Pkg.lib ~exts:Exts.library "src/BetterErrors";
     Pkg.lib ~exts:(Exts.exts [".cmo"; ".cmx"; ".cmt"; ".cmi"]) "src/betterErrorsTypes";
+    Pkg.lib ~exts:(Exts.exts [".cmo"; ".cmx"; ".cmt"; ".cmi"]) "src/betterErrors";
     Pkg.lib ~exts:(Exts.exts [".cmo"; ".cmx"; ".cmt"; ".cmi"]) "src/betterErrorsParseError";
     Pkg.lib ~exts:(Exts.exts [".cmo"; ".cmx"; ".cmt"; ".cmi"]) "src/betterErrorsMain";
     Pkg.lib ~exts:(Exts.exts [".cmo"; ".cmx"; ".cmt"; ".cmi"]) "src/helpers";
     Pkg.lib ~exts:(Exts.exts [".cmo"; ".cmx"; ".cmt"; ".cmi"]) "src/terminalReporter";
     Pkg.lib ~exts:(Exts.exts [".cmo"; ".cmx"; ".cmt"; ".cmi"]) "src/reportWarning";
     Pkg.lib ~exts:(Exts.exts [".cmo"; ".cmx"; ".cmt"; ".cmi"]) "src/reportError";
+    Pkg.lib ~exts:(Exts.exts [".cmo"; ".cmx"; ".cmt"; ".cmi"]) "src/parseError";
     Pkg.lib ~exts:(Exts.exts [".cmo"; ".cmx"; ".cmt"; ".cmi"]) "src/parseWarning";
     Pkg.lib ~exts:(Exts.exts [".cmo"; ".cmx"; ".cmt"; ".cmi"]) "src/Atom";
-    Pkg.lib ~exts:(Exts.exts [".cmo"; ".cmx"; ".cmt"; ".cmi"]) "src/NuclideReporter";
     Pkg.doc "README.md";
   ]
